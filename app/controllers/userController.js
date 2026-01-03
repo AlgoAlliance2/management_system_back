a59abfd9ae4596dfbc063970cc4b6a8f5267904b
Cioban_Daniel <daniel.cioban@student.usv.ro>
2026-01-03 16:18:11 +0200

    Creare functionalitate getAllUsers

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -1,41 +1,18 @@
 const User = require('../models/User');
-const mongoose = require('mongoose'); // Adaugam importul mongoose
 
 exports.getAllUsers = async (req, res) => {
     try {
-        const users = await User.find().select('-password');
+        //GET all users, excluding password for security
+        const users = await User.find().select('-password').lean();
+        
+        // Extra test to ensure users were found
+        if (!users) {
+             return res.status(404).json({ message: 'No users found' });
+        }
+
         res.status(200).json(users);
     } catch (error) {
-        console.error("Error fetching users:", error); // Log error
+        console.error("Error fetching users:", error);
         res.status(500).json({ message: 'Error fetching users', error: error.message });
-    }
-};
-
-exports.updateRole = async (req, res) => {
-    try {
-        const { role } = req.body;
-        const { id } = req.params;
-
-        // 1. Validare ID
-        if (!mongoose.Types.ObjectId.isValid(id)) {
-            return res.status(400).json({ message: 'Invalid User ID format' });
-        }
-
-        // 2. Validare Rol
-        const validRoles = ['student', 'professor', 'organizer', 'admin'];
-        if (!validRoles.includes(role)) {
-            return res.status(400).json({ message: 'Invalid role' });
-        }
-
-        const user = await User.findByIdAndUpdate(id, { role }, { new: true }).select('-password');
-        
-        if (!user) {
-            return res.status(404).json({ message: 'User not found' });
-        }
-
-        res.status(200).json(user);
-    } catch (error) {
-        console.error("Error updating role:", error); // Log error in terminal
-        res.status(500).json({ message: 'Error updating user role', error: error.message });
     }
 };
--- app/routes/userRoutes.js
+++ app/routes/userRoutes.js
@@ -3,13 +3,7 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 const authMiddleware = require('../middleware/authMiddleware');
 
-
-
 // Fetch all users, Auth is Required to see all users
 router.get('/', authMiddleware.requireAuth, userController.getAllUsers);
-
-
-//Update a user's role, Auth is Required to edit an user
-router.patch('/:id/role', authMiddleware.requireAuth, userController.updateRole);
 
 module.exports = router;
