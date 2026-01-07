8b19dd58f3c84331bfd02af5c56191d970431863
Alverot <aliomelinte@gmail.com>
2026-01-07 16:10:09 +0200

    Am reparat problema cu schimbarea rolului de utilizator, getAllUsers trimitea _id dar frontendul astepta id asa ca am modificat formatul in care este trimisa informatia.

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -3,15 +3,20 @@ const User = require('../models/User');
 
 exports.getAllUsers = async (req, res) => {
     try {
-        //GET all users, excluding password for security
         const users = await User.find().select('-password').lean();
         
-        // Extra test to ensure users were found
-        if (!users) {
+        if (!users || users.length === 0) {
              return res.status(404).json({ message: 'No users found' });
         }
 
-        res.status(200).json(users);
+        
+        const formattedUsers = users.map(user => ({
+            ...user,
+            id: user._id.toString(),
+            _id: undefined  
+        }));
+
+        res.status(200).json(formattedUsers);
     } catch (error) {
         console.error("Error fetching users:", error);
         res.status(500).json({ message: 'Error fetching users', error: error.message });
@@ -23,7 +28,7 @@ exports.updateUserRole = async (req, res) => {
         const { role: newRole } = req.body; // The new role
         const requesterId = req.userId; // The admin performing the action
 
-        const validRoles = ['student', 'organizer', 'admin'];
+        const validRoles = ['student', 'professor' ,'organizer', 'admin'];
         if (!validRoles.includes(newRole)) {
             return res.status(400).json({ message: "Invalid role specified." });
         }
